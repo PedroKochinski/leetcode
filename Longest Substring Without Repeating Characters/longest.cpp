55a942f6a1043ce54a1d3b2a25e796cc4c679443
phks20 <phks20@inf.ufpr.br>
2025-01-16 14:13:53 -0300

    [add]: medium longest substring using sliding window

---
+++ Longest Substring Without Repeating Characters/longest.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> charSet;
+        int max = 0;
+        int left = 0;
+        for(int right = 0; right < s.size(); right++){ // O(n)
+            while(charSet.find(s[right]) != charSet.end()){ // found element, worst case it is the last one so we must delete every other element in the set until we find the repeated char, so it is O(n)
+                charSet.erase(s[left]);
+                left+=1;
+            }
+            charSet.insert(s[right]);
+            if(right-left+1 >= max) max = right-left+1;
+        }
+        //total complexity = O(2n) = O(n)
+    return max;
+    }
+};
